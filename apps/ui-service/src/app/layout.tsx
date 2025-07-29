088a387346c4a8d93d16c21776b6be5e9481eec9
arnab <arnhazra@gmail.com>
2025-07-29 22:22:30 +0530

    Publish App

--- apps/ui-service/src/app/layout.tsx
+++ apps/ui-service/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <title>{appName}</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         />
         <meta name="theme-color" content="#18181b" />
         <meta name="description" content={uiConstants.homeIntro} />
