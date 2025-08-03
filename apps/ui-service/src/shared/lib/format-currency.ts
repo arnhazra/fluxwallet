23f0370df6ca2b478580d26bbdc150eab1923634
arnab <arnhazra@gmail.com>
2025-08-03 17:41:35 +0530

    Publish App

--- apps/ui-service/src/shared/lib/format-currency.ts
+++ apps/ui-service/src/shared/lib/format-currency.ts
@@ -1,10 +1,13 @@
 import { Currency } from "../types"
 
+const MAX_STANDARD_AMOUNT = 999999
+
 export function formatCurrency(amount: number, currency: Currency): string {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: currency,
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    currency,
+    notation: amount > MAX_STANDARD_AMOUNT ? "compact" : "standard",
+    minimumFractionDigits: amount > MAX_STANDARD_AMOUNT ? 2 : 0,
+    maximumFractionDigits: amount > MAX_STANDARD_AMOUNT ? 2 : 0,
   }).format(amount)
 }
