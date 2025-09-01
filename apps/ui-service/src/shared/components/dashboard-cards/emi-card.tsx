7adafdbb3c0ad6a5a9e80431de586808cb8f26a0
arnab <arnhazra@gmail.com>
2025-09-01 16:30:19 +0530

    Publish App

--- apps/ui-service/src/shared/components/dashboard-cards/emi-card.tsx
+++ apps/ui-service/src/shared/components/dashboard-cards/emi-card.tsx
@@ -2,7 +2,7 @@
 import { useAppContext } from "@/context/appstate.provider"
 import { Card, CardContent } from "@/shared/components/ui/card"
 import { formatCurrency } from "@/shared/lib/format-currency"
-import { Calculator, CalendarClock } from "lucide-react"
+import { CalendarClock } from "lucide-react"
 import IconContainer from "../icon-container"
 import { endPoints } from "@/shared/constants/api-endpoints"
 import HTTPMethods from "@/shared/constants/http-methods"
