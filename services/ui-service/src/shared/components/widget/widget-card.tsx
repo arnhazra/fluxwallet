f944997a934ac9a3711acd03c0446c51924d2f97
arnab <arnhazra@gmail.com>
2025-12-08 18:02:18 +0530

    Publish App

--- services/ui-service/src/shared/components/widget/widget-card.tsx
+++ services/ui-service/src/shared/components/widget/widget-card.tsx
@@ -22,14 +22,14 @@ export default function WidgetCard({
   const [{ user }] = useUserContext()
 
   return (
-    <Card className="bg-background border border-border rounded-3xl relative overflow-hidden hover:shadow-md hover:shadow-primary/20">
+    <Card className="bg-background/2 backdrop-blur-sm border border-border rounded-3xl relative overflow-hidden hover:shadow-md hover:shadow-primary/20">
       <CardContent>
-        <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center justify-between mb-2 -mt-2">
           <span className="text-sm text-neutral-300">{statTitle}</span>
           <IconContainer>{icon}</IconContainer>
         </div>
         <div className="space-y-3">
-          <p className="text-3xl font-bold text-white">
+          <p className="text-4xl font-bold text-white">
             {formatCurrency(statValue, user.baseCurrency)}
           </p>
           <div className="mt-2">
