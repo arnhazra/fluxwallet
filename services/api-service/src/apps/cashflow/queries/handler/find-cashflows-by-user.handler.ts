67ee1e463acd6bfc20d1c03a4309f468628addf5
arnab <arnhazra@gmail.com>
2025-12-16 17:26:56 +0530

    Publish App

--- services/api-service/src/apps/cashflow/queries/handler/find-cashflows-by-user.handler.ts
+++ services/api-service/src/apps/cashflow/queries/handler/find-cashflows-by-user.handler.ts
@@ -1,17 +1,27 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs"
 import { FindCashflowsByUserQuery } from "../impl/find-cashflows-by-user.query"
 import { CashFlowRepository } from "../../cashflow.repository"
-import { createOrConvertObjectId } from "@/shared/entity/entity.schema"
+import {
+  createOrConvertObjectId,
+  QueryFilter,
+} from "@/shared/entity/entity.schema"
+import { Cashflow } from "../../schemas/cashflow.schema"
 
 @QueryHandler(FindCashflowsByUserQuery)
 export class FindCashflowsByUserQueryHandler implements IQueryHandler<FindCashflowsByUserQuery> {
   constructor(private readonly repository: CashFlowRepository) {}
 
   async execute(query: FindCashflowsByUserQuery) {
-    const { userId } = query
+    const { userId, searchKeyword } = query
 
-    return this.repository.find({
+    const filter: QueryFilter<Cashflow> = {
       userId: createOrConvertObjectId(userId),
-    })
+    }
+
+    if (searchKeyword && searchKeyword.trim().length > 0) {
+      filter.description = { $regex: new RegExp(searchKeyword, "i") }
+    }
+
+    return this.repository.find(filter)
   }
 }
