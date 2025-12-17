cc9768ba0db14e9f5a3c84cd5cbc253be6df9b6e
arnab <arnhazra@gmail.com>
2025-12-17 14:57:33 +0530

    Publish App

--- services/api-service/src/main.ts
+++ services/api-service/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core"
 import { MainModule } from "./main.module"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
-import { config } from "./config"
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(MainModule)
