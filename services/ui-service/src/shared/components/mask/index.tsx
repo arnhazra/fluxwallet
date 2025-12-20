d5322de21c2a05cf09d8be4c4f6ca97125e8f7c9
arnab <arnhazra@gmail.com>
2025-12-20 14:46:20 +0530

    Publish App

--- services/ui-service/src/shared/components/mask/index.tsx
+++ services/ui-service/src/shared/components/mask/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 export default function MaskText({ value }: { value: string }) {
-  if (!value || value.length <= 6) {
+  if (!value || value.length <= 8) {
     return <span>{value}</span>
   }
   return `${value?.substring(0, 3)}...${value?.substring(value?.length - 3)}`
