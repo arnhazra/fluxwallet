8f9b50d0fbb586ec6b1c709d889773033cc2bc16
arnab <arnhazra@gmail.com>
2025-12-26 12:04:56 +0530

    Publish App

--- services/cron-service/src/app/api/cron/route.ts
+++ services/cron-service/src/app/api/cron/route.ts
@@ -6,8 +6,25 @@ export async function GET(request: Request) {
   const res = await ky.get(`${API_URL}/apps/cashflow/execute`)
 
   if (!res.ok) {
-    return new Response("Email API failed", { status: 500 })
+    return new Response("Cron execution failed", {
+      status: 500,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+        "Surrogate-Control": "no-store",
+      },
+    })
   }
 
-  return new Response("Cron executed successfully", { status: 200 })
+  return new Response("Cron executed successfully", {
+    status: 200,
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+      "Surrogate-Control": "no-store",
+    },
+  })
 }
