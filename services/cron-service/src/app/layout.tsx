f3bf59b9c43b44d64f601577118bbdc91769af31
arnab <arnhazra@gmail.com>
2025-12-26 11:12:02 +0530

    Publish App

--- services/cron-service/src/app/layout.tsx
+++ services/cron-service/src/app/layout.tsx
@@ -7,7 +7,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Ok</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+        <meta name="theme-color" content="#121212" />
+      </head>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
