d74ef409615cded2762acd8262b6ee7c3c25c14e
arnab <arnhazra@gmail.com>
2026-01-06 14:11:17 +0530

    Publish App

--- services/ui-service/src/shared/components/app-card/index.tsx
+++ services/ui-service/src/shared/components/app-card/index.tsx
@@ -1,9 +1,9 @@
 import * as Icons from "lucide-react"
 import { App } from "@/shared/constants/types"
 import IconContainer from "../icon-container"
-import { useRouter } from "nextjs-toploader/app"
 import { platformName } from "@/shared/constants/global-constants"
 import { Card, CardContent, CardHeader } from "../ui/card"
+import Link from "next/link"
 
 interface AppCardProps {
   app: App
@@ -11,25 +11,23 @@ interface AppCardProps {
 
 export function AppCard({ app }: AppCardProps) {
   const AppIcon = (Icons as any)[app.icon] || Icons.HelpCircle
-  const router = useRouter()
 
   return (
-    <Card
-      className="bg-background border border-border p-2 rounded-3xl hover:shadow-lg hover:shadow-primary/20 cursor-pointer"
-      onClick={() => router.push(app.url)}
-    >
-      <CardHeader className="flex justify-between mt-6 items-center">
-        <div>
-          <p className="text-xs">{platformName}</p>
-          <h2 className="text-xl">{app.displayName}</h2>
-        </div>
-        <IconContainer>
-          <AppIcon className="h-5 w-5" />
-        </IconContainer>
-      </CardHeader>
-      <CardContent className="mb-6">
-        <p className="text-sm leading-relaxed justify">{app.description}</p>
-      </CardContent>
-    </Card>
+    <Link href={app.url}>
+      <Card className="bg-background border border-border p-2 rounded-3xl hover:shadow-lg hover:shadow-primary/20 cursor-pointer">
+        <CardHeader className="flex justify-between mt-6 items-center">
+          <div>
+            <p className="text-xs">{platformName}</p>
+            <h2 className="text-xl">{app.displayName}</h2>
+          </div>
+          <IconContainer>
+            <AppIcon className="h-5 w-5" />
+          </IconContainer>
+        </CardHeader>
+        <CardContent className="mb-6">
+          <p className="text-sm leading-relaxed justify">{app.description}</p>
+        </CardContent>
+      </Card>
+    </Link>
   )
 }
